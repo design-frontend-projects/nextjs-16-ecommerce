aeee1a2803551bc82724d0711667feae4a443b00
ahmedabdelhafez <messi10010@gmail.com>
2026-01-30 17:38:25 +0200

    feat: Add new agent skills, rules, and their associated resources.

--- src/components/features/ProductsFeature/ProductsMain.tsx
+++ src/components/features/ProductsFeature/ProductsMain.tsx
@@ -11,6 +11,7 @@ import ProductsSectionTabs from '@/components/features/MainPageProductsComponent
 import Image from 'next/image';
 import OurCustomerCare from '../MainPageProductsComponents/OurCustomerCare';
 import ProductModelCard from '../MainPageProductsComponents/ProductModelCard';
+import { useAuth, useClerk, useUser } from '@clerk/nextjs';
 // import ProductsDataFetch from './products-data-fetch/ProductsDataFetch';
 
 const ProductsMain = () => {
@@ -18,6 +19,11 @@ const ProductsMain = () => {
     initial: { opacity: 0, y: 20 },
     animate: { opacity: 1, y: 0, transition: { duration: 0.6 } },
   };
+
+  const { user } = useUser();
+  const { has, isSignedIn } = useAuth();
+  console.log('user data in products main: ');
+  console.log(user);
 
   return (
     <>
